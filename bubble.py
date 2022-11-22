29aa24889d66c7e842c4fe03bf0ed33806e6e86d
OjanRN <ojanrn5@gmail.com>
2022-11-22 19:02:10 +0700

    Renamed 1 variable

--- bubble.py
+++ bubble.py
@@ -1,21 +1,17 @@
 #Bubble sort algorithm by OjanRN
 #Github: github.com/ojanrn
 
-import time
-
-def mainApp():
-    start = time.time()
-    inputList = [85,32,42,12,95,20,40]
+def mainapp():
+    inputlist = [85,32,42,12,95,20,40]
     order = 0
     while True:
-        if order+1 == len(inputList):
+        if order+1 == len(inputlist):
             order = 0
-        if inputList[order] > inputList[order + 1]:
-            inputList[order + 1], inputList[order] = inputList[order], inputList[order + 1]
+        if inputlist[order] > inputlist[order + 1]:
+            inputlist[order + 1], inputlist[order] = inputlist[order], inputlist[order + 1]
+            print(inputlist[order], inputlist[order + 1])
         order+= 1
-        print(inputList)
-        if inputList == sorted(inputList):
-            end = time.time()
+        print(inputlist)
+        if inputlist == sorted(inputlist):
             break
-
-mainApp()
+mainapp()
