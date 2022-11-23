ae701c00dd60fd9abca257f1fc7a170de166c29d
OjanRN <ojanrn5@gmail.com>
2022-11-23 19:21:11 +0700

    removed useless stuff

--- bubble.py
+++ bubble.py
@@ -2,16 +2,16 @@
 #Github: github.com/ojanrn
 
 def mainapp():
-    inputlist = [85,32,42,12,95,20,40]
+    inputlist = [3,5,2,23,15,63,100]
     order = 0
+    print(inputlist)
     while True:
         if order+1 == len(inputlist):
             order = 0
         if inputlist[order] > inputlist[order + 1]:
             inputlist[order + 1], inputlist[order] = inputlist[order], inputlist[order + 1]
-            print(inputlist[order], inputlist[order + 1])
         order+= 1
-        print(inputlist)
         if inputlist == sorted(inputlist):
+            print(inputlist)
             break
 mainapp()
